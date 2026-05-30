b1257bdbaab973249aa4b4e8b80e61f742674847
bagdroplogistics-create <bagdroplogistics@gmail.com>
2026-05-30 17:28:42 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button';
 // import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 import { useNavigate, useLocation } from 'react-router-dom';
 import TrackingSection from './TrackingSection';
-import BookingModal from "./components/BookingModal";
+import BookingModal from "./BookingModal";
 
 const Header = ({ onBookNow }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
